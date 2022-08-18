ce24eaa86e8164decafc5f9b5cb81c80a6b81e43
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-08-18 19:02:39 +0530

    lit function added

---
+++ src/main/java/io/infoworks/customers/att/udfs/LitHiveUDF.java
@@ -0,0 +1,14 @@
+package io.infoworks.customers.att.udfs;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class LitHiveUDF extends UDF {
+
+  public static Object evaluate(Object input) {
+    return input;
+  }
+}
