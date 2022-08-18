56e2be1fd89b41707751599d819a5214f4653d1e
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-08-18 22:59:38 +0530

    fix

--- src/main/java/io/infoworks/customers/att/udfs/CoalesceAsCharHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/CoalesceAsCharHiveUDF.java
@@ -1,11 +1,15 @@
 package io.infoworks.customers.att.udfs;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class CoalesceAsCharHiveUDF extends UDF {
 
-  public static String evaluate(String input, int size, Object defaultInput) {
-    String coalescedObject = (String) Utils.coalesce(input, defaultInput);
+  public static String evaluate(String input, Integer size, String defaultInput) {
+    String coalescedObject = Utils.coalesce(input, defaultInput);
+    if (StringUtils.isEmpty(coalescedObject)) {
+      return coalescedObject;
+    }
     return coalescedObject.substring(0, size);
   }
 }
