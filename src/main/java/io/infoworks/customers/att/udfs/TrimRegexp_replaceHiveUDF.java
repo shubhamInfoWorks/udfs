c1751d58cf5190598224369b566d62b03f955079
Infoworks <aditya@infoworks.io>
2022-08-22 18:01:21 +0530

    Renamed NonASCIICleanupHiveUDF to TrimRegexp_replaceHiveUDF

--- src/main/java/io/infoworks/customers/att/udfs/TrimRegexp_replaceHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/TrimRegexp_replaceHiveUDF.java
@@ -1,11 +1,9 @@
 package io.infoworks.customers.att.udfs;
 
-import java.io.Serializable;
-import java.util.function.Function;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.datanucleus.util.StringUtils;
 
-public class NonASCIICleanupHiveUDF extends UDF {
+public class TrimRegexp_replaceHiveUDF extends UDF {
 
 
   public static String evaluate(String unTransformed) {
