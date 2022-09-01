77b4709cd805be90e2ab7a1fd53b2571a3a69b97
Darshan Venkatesh <darshan.venkatesh@infoworks.io>
2022-09-01 12:45:37 +0530

    Changed date formats and built jar

--- src/main/java/io/infoworks/customers/att/udfs/CastNullIFtoDateFormatHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/CastNullIFtoDateFormatHiveUDF.java
@@ -3,17 +3,19 @@ package io.infoworks.customers.att.udfs;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.apache.commons.lang.StringUtils;
 
 public class CastNullIFtoDateFormatHiveUDF {
-  public static String evaluate(String input, String secondexpression,String dateformat) throws ParseException {
+  public static LocalDate evaluate(String input, String secondexpression,String dateformat) throws ParseException {
 
     if (StringUtils.isEmpty(input)) {
       return null;
     }
 
     DateFormat dateFormat = new SimpleDateFormat(dateformat);
-    return dateFormat.format(new SimpleDateFormat(dateformat).parse(NullIFHiveUDF.evaluate(input, secondexpression)).getTime());
+    return LocalDate.parse(NullIFHiveUDF.evaluate(input, secondexpression), DateTimeFormatter.ofPattern(dateformat));
 
   }
 
