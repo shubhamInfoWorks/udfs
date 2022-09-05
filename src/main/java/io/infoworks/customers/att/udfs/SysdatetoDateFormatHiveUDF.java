3f5926ef19b210df5dc2001e2e070efceffc72b3
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-05 18:49:11 +0530

    fix

--- src/main/java/io/infoworks/customers/att/udfs/SysdatetoDateFormatHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/SysdatetoDateFormatHiveUDF.java
@@ -1,19 +1,12 @@
 package io.infoworks.customers.att.udfs;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
+import java.sql.Date;
+import java.util.Calendar;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class SysdatetoDateFormatHiveUDF extends UDF {
 
-    public static Date evaluate(Date input)
-    {
-
-
-
-        return new Date();
-
-    }
+  public static Date evaluate(Date input) {
+    return new java.sql.Date(Calendar.getInstance().getTime().getTime());
+  }
 }
