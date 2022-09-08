bf81dfd044b4c14a1e10c2d1d76e6cebaf2e3e26
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-08 14:53:12 +0530

    nvl hive

---
+++ src/main/java/io/infoworks/customers/att/udfs/NvlHiveUDF.java
@@ -0,0 +1,43 @@
+package io.infoworks.customers.att.udfs;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class NvlHiveUDF extends UDF {
+
+  public static Double evaluate(String input, Double defaultInput) {
+    if (input != null) {
+      return Double.valueOf(input);
+    }
+    return defaultInput;
+  }
+
+  public static Long evaluate(String input, Long defaultInput) {
+    if (input != null) {
+      return Long.valueOf(input);
+    }
+
+    return defaultInput;
+  }
+
+  public static Integer evaluate(String input, Integer defaultInput) {
+    if (input != null) {
+      return Integer.valueOf(input);
+    }
+    return defaultInput;
+  }
+
+  public static Float evaluate(String input, Float defaultInput) {
+    if (input != null) {
+      return Float.valueOf(input);
+    }
+    return defaultInput;
+  }
+
+  public static String evaluate(String input, String defaultInput) {
+    if (input != null) {
+      return input;
+    }
+    return defaultInput;
+  }
+
+}
