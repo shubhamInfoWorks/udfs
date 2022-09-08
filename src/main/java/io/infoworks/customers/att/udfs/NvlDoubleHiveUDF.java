3088023e311e9712ce40f5ab9c1021ce197dc82a
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-08 13:50:48 +0530

    adding the all datatype into 1

--- src/main/java/io/infoworks/customers/att/udfs/NvlDoubleHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/NvlDoubleHiveUDF.java
@@ -4,11 +4,32 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class NvlDoubleHiveUDF extends UDF {
 
-    public static Double evaluate(String input, Double defaultInput) {
-        if (input != null) {
-            return Double.valueOf(input);
-        }
-
-        return defaultInput;
+  public static Double evaluate(String input, Double defaultInput) {
+    if (input != null) {
+      return Double.valueOf(input);
     }
+    return defaultInput;
+  }
+
+  public static Long evaluate(String input, Long defaultInput) {
+    if (input != null) {
+      return Long.valueOf(input);
+    }
+
+    return defaultInput;
+  }
+
+  public static Integer evaluate(String input, Integer defaultInput) {
+    if (input != null) {
+      return Integer.valueOf(input);
+    }
+    return defaultInput;
+  }
+
+  public static Float evaluate(String input, Float defaultInput) {
+    if (input != null) {
+      return Float.valueOf(input);
+    }
+    return defaultInput;
+  }
 }
