dc835897352f6935a884554329fc29b453583436
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-09 17:59:14 +0530

    fix

--- src/main/java/io/infoworks/customers/att/udfs/TimeAsString.java
+++ src/main/java/io/infoworks/customers/att/udfs/TimeAsString.java
@@ -1,13 +1,14 @@
 package io.infoworks.customers.att.udfs;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.time.format.DateTimeFormatter;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class TimeAsString extends UDF {
 
-  public static String evaluate(String input, String format) throws ParseException {
-    return CastStringToTimestampFormatHiveUDF.evaluate(input, format).toLocalDateTime().format(
+  public static String evaluate(String input) throws ParseException {
+    return new Timestamp(System.currentTimeMillis()).toLocalDateTime().format(
         DateTimeFormatter.ofPattern("HH:mm:ss"));
   }
 }
