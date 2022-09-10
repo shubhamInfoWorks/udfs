b28b53134a4d52f4b5f81e33661128a98ceff1e0
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-11 01:45:57 +0530

    date accept

--- src/main/java/io/infoworks/customers/att/udfs/TimestampTransformerHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/TimestampTransformerHiveUDF.java
@@ -1,11 +1,16 @@
 package io.infoworks.customers.att.udfs;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class TimestampTransformerHiveUDF extends UDF {
 
   public static Timestamp evaluate(Timestamp untransformed) {
+    return new Timestamp(System.currentTimeMillis());
+  }
+
+  public static Timestamp evaluate(Date untransformed) {
     return new Timestamp(System.currentTimeMillis());
   }
 }
