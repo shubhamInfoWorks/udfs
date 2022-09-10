5770fceb584056c012ff0c85b51c6d7ccc15b64d
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-10 13:53:41 +0530

    changes

--- src/main/java/io/infoworks/customers/att/udfs/CoalesceNullIFHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/CoalesceNullIFHiveUDF.java
@@ -1,25 +1,37 @@
 package io.infoworks.customers.att.udfs;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class CoalesceNullIFHiveUDF extends UDF {
 
-  public static long evaluate(String input, String secondexpression, String defaultinput) {
-
+  public static Long evaluate(String input, String secondExpression, Long defaultInput) {
     try {
       return Long.parseLong(
-          Utils.coalesce(NullIFHiveUDF.evaluate(input, secondexpression), defaultinput));
+          Utils.coalesce(NullIFHiveUDF.evaluate(input, secondExpression), defaultInput.toString()));
     } catch (Exception e) {
+      return 0L;
+    }
+  }
 
+  public static Double evaluate(String input, String secondExpression, Double defaultInput) {
+    try {
+      return Double.parseDouble(
+          Utils.coalesce(NullIFHiveUDF.evaluate(input, secondExpression), defaultInput.toString()));
+    } catch (Exception e) {
+      return 0.0;
+    }
+  }
+
+  public static Integer evaluate(String input, String secondExpression, Integer defaultInput) {
+    try {
+      return Integer.parseInt(
+          Utils.coalesce(NullIFHiveUDF.evaluate(input, secondExpression), defaultInput.toString()));
+    } catch (Exception e) {
       return 0;
     }
   }
 
-  public static long evaluate(long input, String secondexpression, long defaultinput) {
-
-    //   return Utils.coalesce(NullIFHiveUDF.evaluate(input, secondexpression), defaultinput);
-
-    return ObjectUtils.firstNonNull(NullIFHiveUDF.evaluate(input, secondexpression), defaultinput);
+  public static String evaluate(String input, String secondExpression, String defaultInput) {
+    return Utils.coalesce(NullIFHiveUDF.evaluate(input, secondExpression), defaultInput);
   }
 }
--- src/main/java/io/infoworks/customers/att/udfs/Test.java
+++ src/main/java/io/infoworks/customers/att/udfs/Test.java
@@ -45,7 +45,7 @@ public class Test {
     // System.out.println(StringToTimestampHiveUDF.evaluate("09B11A2024 22:59:09"));
     // System.out.println(SysdatetoIntHiveUDF.evaluate(2));
     System.out.println(CoalesceNullIFHiveUDF.evaluate("234", "23", "67"));
-    System.out.println(CoalesceNullIFHiveUDF.evaluate(2, "", 6));
+    System.out.println(CoalesceNullIFHiveUDF.evaluate("2", "2", 67L));
 
     // System.out.println(NULLIFHiveTimestampUDF.evaluate(Timestamp.valueOf("2022-03-02
     // 22:34:31"),Timestamp.valueOf("2022-03-02 22:34:32")));
@@ -85,6 +85,6 @@ public class Test {
 
     // System.out.println(StringtoDateFormatHiveUDF.evaluate("220405","19","yyyyMMdd"));
     //  System.out.println(StringtoDateFormatHiveUDF.evaluate("200405","20","yyyyMMdd"));
-    System.out.println(TimeAsString.evaluate(null));
+    //System.out.println(TimeAsString.evaluate(null));
   }
 }
