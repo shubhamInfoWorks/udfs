eb0aea9f55db1c23227543c08e8ed7846a2810ea
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-11 14:07:34 +0530

    date added

--- src/main/java/io/infoworks/customers/att/udfs/NullIFHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/NullIFHiveUDF.java
@@ -1,5 +1,6 @@
 package io.infoworks.customers.att.udfs;
 
+import java.sql.Date;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
@@ -16,12 +17,24 @@ public class NullIFHiveUDF extends UDF {
     return input;
   }
 
-  public static long evaluate(long input, String secondexpression) {
-
-    if (String.valueOf(input).equals(String.valueOf(secondexpression))) {
-      return 0;
+  public static Long evaluate(Long input, String secondExpression) {
+    if (String.valueOf(input).equals(String.valueOf(secondExpression))) {
+      return null;
     }
+    return input;
+  }
 
+  public static Integer evaluate(Integer input, String secondExpression) {
+    if (String.valueOf(input).equals(String.valueOf(secondExpression))) {
+      return null;
+    }
+    return input;
+  }
+
+  public static Date evaluate(Date input, String secondExpression) {
+    if (String.valueOf(input).equals(secondExpression)) {
+      return null;
+    }
     return input;
   }
 }
