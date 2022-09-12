27d4057d6eb173bfa45e7f80410d26a2349c85ad
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-12 23:55:34 +0530

    added bigdecimal support

--- src/main/java/io/infoworks/customers/att/udfs/NvlDoubleHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/NvlDoubleHiveUDF.java
@@ -1,5 +1,6 @@
 package io.infoworks.customers.att.udfs;
 
+import java.math.BigDecimal;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
 public class NvlDoubleHiveUDF extends UDF {
@@ -9,6 +10,14 @@ public class NvlDoubleHiveUDF extends UDF {
       return Double.valueOf(input);
     }
     return defaultInput;
+  }
+
+  public static BigDecimal evaluate(String input, BigDecimal defaultInput) {
+    BigDecimal output = defaultInput;
+    if (input != null) {
+      output = new BigDecimal(Double.valueOf(input));
+    }
+    return output;
   }
 
   public static Long evaluate(String input, Long defaultInput) {
