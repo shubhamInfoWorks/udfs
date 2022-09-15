310bd2b1f9bdacf35dad349d11bda8ddfcd8877f
shubhamInfoWorks <47854756+shubhamInfoWorks@users.noreply.github.com>
2022-09-15 15:14:53 +0530

    string from date

--- src/main/java/io/infoworks/customers/att/udfs/SysdatetoDateFormatHiveUDF.java
+++ src/main/java/io/infoworks/customers/att/udfs/SysdatetoDateFormatHiveUDF.java
@@ -1,6 +1,7 @@
 package io.infoworks.customers.att.udfs;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
@@ -8,5 +9,11 @@ public class SysdatetoDateFormatHiveUDF extends UDF {
 
   public static Date evaluate(Date input) {
     return new java.sql.Date(Calendar.getInstance().getTime().getTime());
+  }
+
+  public static String evaluate(String input) {
+    SimpleDateFormat parseFormat = new SimpleDateFormat("yyyy-MM-dd");
+    Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+    return parseFormat.format(date);
   }
 }
